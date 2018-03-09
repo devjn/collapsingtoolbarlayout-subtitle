d7cda50813cad6fda89986d59bc07f148209c1c7
devjn <devjn@jn-arts.com>
2018-03-09 21:36:42 +0200

    Fix for jitpack?

--- collapsingtoolbarlayout-subtitle/build.gradle.kts
+++ collapsingtoolbarlayout-subtitle/build.gradle.kts
@@ -15,7 +15,6 @@ android {
     defaultConfig {
         minSdkVersion(minSdk)
         targetSdkVersion(targetSdk)
-        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     sourceSets {
         getByName("main") {
@@ -24,27 +23,12 @@ android {
             res.srcDir("res")
             resources.srcDir("src")
         }
-        getByName("androidTest") {
-            setRoot("tests")
-            manifest.srcFile("tests/AndroidManifest.xml")
-            java.srcDir("tests/src")
-            res.srcDir("tests/res")
-            resources.srcDir("tests/src")
-        }
     }
 }
 
 dependencies {
     compile(kotlin("stdlib", kotlinVersion))
     compile(support("design", supportVersion))
-
-    testImplementation(junit(junitVersion))
-    testImplementation(google("truth", truthVersion))
-
-    androidTestImplementation(google("truth", truthVersion))
-    androidTestImplementation(hendraanggrian("errorbar", errorbarVersion))
-    androidTestImplementation(support("runner", runnerVersion, "test"))
-    androidTestImplementation(support("espresso-core", espressoVersion, "test", "espresso"))
 }
 
 publish {
